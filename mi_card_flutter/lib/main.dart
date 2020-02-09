c7e83b359aa6d3c152605050546d0450f7afb8a9
Fernando Batista <fernandohsbatista@gmail.com>
2020-02-09 11:20:18 -0300

    Finished first layout challenge on mi card app.

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -1,17 +1,66 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ChallengeApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        body: Container(),
+        home: Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                height: double.infinity,
+              ),
+              Container(color: Colors.white, margin: EdgeInsets.all(4)),
+            ]),
       ),
-    );
+    ));
+  }
+}
+
+class ChallengeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              width: 100.0,
+              color: Colors.red,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 100.0,
+                  height: 100.0,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  width: 100.0,
+                  height: 100.0,
+                  color: Colors.green,
+                ),
+              ],
+            ),
+            Container(
+              width: 100.0,
+              color: Colors.blue,
+            )
+          ],
+        ),
+      ),
+    ));
   }
 }
